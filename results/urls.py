8456855940a7801c13daf55a8e32d6b1aeba9933
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2021-10-18 09:38:28 +0500

    natijalar ko'rsatish hamda qidirish ishlayapti

--- results/urls.py
+++ results/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.results_dashboard, name='results_dashboard'),
+    path('<int:type_task>/', views.results_table, name='table'),
     path('json', views.search_pupil_result),
 ]
--- results/views.py
+++ results/views.py
@@ -11,16 +11,30 @@ def results_dashboard(request):
     return render(request, 'results/results_dashboard.html')
 
 
-def results_table(request):
+def results_table(request, type_task):
 
-    allFirstTaskResults = AllResult.objects.filter(type_task=1)
-    allSecondTaskResults = AllResult.objects.filter(type_task=2)
-    allThirdTaskResults = AllResult.objects.filter(type_task=3)
+    taskResults = AllResult.objects.filter(
+        type_task=type_task).order_by('-avarage_score')
+    my_result, created = AllResult.objects.get_or_create(
+        pupil=request.user, type_task=type_task)
+
+    text = request.GET.get('searching_text')
+
+    if text != None:
+        taskResults = taskResults.filter(pupil__first_name__contains=text)
+
+    context = {
+        'task_results': taskResults,
+        'my_result': my_result,
+    }
+
+    return render(request, 'results/index.html', context)
 
 
 def search_pupil_result(request):
 
-    searchResult = AllResult.objects.filter(pupil__first_name__contains="Oy").filter(type_task=2)
+    searchResult = AllResult.objects.filter(
+        pupil__first_name__contains="Oy").filter(type_task=2)
 
     print(searchResult)
     return HttpResponse("1")
